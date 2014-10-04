2cd48c18d525a73934295b259f17af462d0bae9d
Vinu Ilangovan <vinuilangovan@Vinus-MacBook-Pro.local>
2014-10-04 15:51:38 -0400

    Initial Commit

---
+++ SmartLock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartLock
+//
+//  Created by Vinu Ilangovan on 10/4/14.
+//  Copyright (c) 2014 Vinu Ilangovan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartLock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartLock
+//
+//  Created by Vinu Ilangovan on 10/4/14.
+//  Copyright (c) 2014 Vinu Ilangovan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
